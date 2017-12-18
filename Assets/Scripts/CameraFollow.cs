8f67c17c33f63768f8cf1afd444e0c98b9f2e98b
dnorthrup3 <dnorthrup3@gatech.edu>
2017-12-17 20:22:20 -0800

    Making camera follow smoother

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,7 @@ public class CameraFollow : MonoBehaviour {
 
   public GameObject target;
   public Vector3 offset;
+  public float speed = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,10 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    transform.position = new Vector3 (
+    Vector3 targetPosition = new Vector3 (
       target.transform.position.x + offset.x, 
       target.transform.position.y + offset.y, 
       transform.position.z);
+    transform.position = Vector3.Lerp (transform.position, targetPosition, speed * Time.deltaTime);
 	}
 }
